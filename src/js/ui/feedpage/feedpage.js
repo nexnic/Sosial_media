9e189703a8befda265eac9cd798faf524ca7393c
Kent Erik <kent.erik.hole@outlook.com>
2023-06-16 21:57:07 +0200

    adding new feature

--- src/js/ui/feedpage/feedpage.js
+++ src/js/ui/feedpage/feedpage.js
@@ -1,8 +1,11 @@
 import { load } from '../../storage/load'
 import { GetallPost } from '../../api/post/getAllpost'
+import { templetnavbar } from '../../templet/navigation/templetnav'
 
 export const pageConfigFeedpage = () => {
 	const token = load('token')
+	const userdata = load('userData')
 	console.log(token)
 	GetallPost(token)
+	templetnavbar(userdata)
 }
