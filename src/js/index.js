45c5b3b6e35aa5830edef54d7e751aa2be00153c
Kent Erik <kent.erik.hole@outlook.com>
2023-06-16 21:56:36 +0200

    adding button

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,19 @@ import ui from './ui/index'
 
 listeners()
 ui()
+
+document.querySelector('#usersBtn')?.addEventListener('click', (event) => {
+	console.log('click')
+})
+
+document.querySelector('#addbtn')?.addEventListener('click', (event) => {
+	console.log('click')
+})
+
+document.querySelector('#searchbtn')?.addEventListener('click', (event) => {
+	console.log('click')
+})
+
+document.querySelector('#homebtn')?.addEventListener('click', () => {
+	console.log('click')
+})
