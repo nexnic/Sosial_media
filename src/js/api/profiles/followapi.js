e0ddd712583c127c6a34802538b2b74ae442bb6d
Kent Erik <kent.erik.hole@outlook.com>
2023-06-17 11:59:31 +0200

    adding following

---
+++ src/js/api/profiles/followapi.js
@@ -0,0 +1,21 @@
+export async function followingAPI(userName, token) {
+	try {
+		const response = await fetch(
+			`https://nf-api.onrender.com/api/v1/social/profiles/${userName}/follow`,
+			{
+				method: 'PUT',
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		)
+		if (response.ok) {
+			if (response.status === 200) {
+				console.log(await response.json())
+			}
+		}
+		console.log(await response.json())
+	} catch (error) {
+		console.log(error)
+	}
+}
